a18366bd146b72c93d486d33f290308349e75530
gianluca <gianluca.mittone@unito.it>
2023-03-17 17:18:12 +0100

    Removed old code

--- unito/openfl_ext/identity.py
+++
@@ -1,9 +0,0 @@
-from openfl.component.aggregation_functions.interface import AggregationFunction
-
-
-class Identity(AggregationFunction):
-    """Identity function for faking aggregation"""
-
-    def call(self, local_tensors, *_):
-        
-        return local_tensors
